feda756834e1243de72765cc9857b5ca3a21bcc6
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2024-10-22 12:07:13 +0600

    blogs json data added

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+                  <h1 className="text-center text-5x font-bold">Inspiration Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
